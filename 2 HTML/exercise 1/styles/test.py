65eaca55e72f3e7ef66c2c962520a0876249568e
destroyer183 <jzante99@gmail.com>
2024-02-13 14:09:13 -0500

    mostly finished exercise 1 and started learning js

--- 2 HTML/exercise 1/styles/test.py
+++
@@ -1,3 +0,0 @@
-import math
-
-print(f"there are {math.floor(375 / 60)} hours, and {375 % 60} minutes.")
---
+++ 3 buttons and javascript/scripts/script.js
@@ -0,0 +1,51 @@
+
+
+alert('Hello World!');
+
+
+
+
+
+const numbers = [1, 2, -2, 6, -7, 9, -4, -8];
+
+console.log('array: ' + numbers.toString());
+
+numbers.sort();
+
+console.log('sorted array: ' + numbers.toString());
+
+console.log('typeof NaN(not a number): \"' + typeof(NaN) + '\"');
+
+console.log('99999999999999999 = ' + 99999999999999999);
+
+console.log('0.5 + 0.1 == 0.6: ' + (0.5 + 0.1 == 0.6));
+
+console.log('0.1 + 0.2 == 0.3: ' + (0.1 + 0.2 == 0.3));
+
+console.log('Math.max(): ' + Math.max());
+
+console.log('Math.min(): ' + Math.min());
+
+console.log('[] + []: \"' + ([] + []) + '\"');
+
+console.log('[] + {}: \"' + ([] + {}) + '\"');
+
+console.log('{} + []: ' + ({} + []));
+
+console.log('true + true + true === 3: ' + (true + true + true === 3));
+
+console.log('true - true: ' + (true - true));
+
+console.log('true == 1: ' + (true == 1));
+
+console.log('true === 1: ' + (true === 1));
+
+console.log('(! + [] + ![]).length: ' + ((! + [] + ![]).length));
+
+console.log('(! + [] + [] + ![]).length: ' + ((! + [] + [] + ![]).length));
+
+console.log('9 + \"1\": \"' + (9 + '1') + '\"');
+
+console.log('91 - \"1\": ' + (91 - '1'));
+
+console.log('[] == 0: ' + ([] == 0));
